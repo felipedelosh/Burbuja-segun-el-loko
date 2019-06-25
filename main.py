03fe03f9fce06779e70046e7490d12fe00522abe
Andres Felipe Herndandez <38327255+felipedelosh@users.noreply.github.com>
2019-06-25 11:20:43 -0500

    Animacion fluida[pedi ayuda en stackoverflow]
    La animacion es fluida

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ FelipedelosH
 
 Se desea hacer un programa que dados 320 numeros aleatoreos de 0 a 400
 los organice por medio de burbuja y muestre paso a paso con un slider que retarde
+
+
+6/25/2019
+
+Acabo de recivir ayuda de abulafia
+https://es.stackoverflow.com/questions/273963/como-se-sincronizan-2-hilos-en-python
 """
 
 from tkinter import *
@@ -12,17 +18,18 @@ from random import randint
 from threading import *
 from time import sleep
 
-class GraphiBubbleShort():
+
+class GraphiBubbleShort:
     def __init__(self):
         self.pantalla = Tk()
-        self.telaControl = Canvas(self.pantalla,width=640, height=80 ,bg="snow")
-        self.telaGraficas = Canvas(self.pantalla,width=640, height=400 ,bg="white")
+        self.telaControl = Canvas(self.pantalla, width=640, height=80, bg="snow")
+        self.telaGraficas = Canvas(self.pantalla, width=640, height=400, bg="white")
         self.btnPlay = Button(self.telaControl, text="PLAY", command=self.play)
         self.btnPausa = Button(self.telaControl, text="PAUSE", command=self.pause)
         self.lblIteracion = Label(self.telaControl, text="Iteracion: 0")
 
         """Variables"""
-        self.vectorNumeros = [] # aqui se guardan los 640 numeros
+        self.vectorNumeros = []  # aqui se guardan los 640 numeros
         self.estoyRondando = False  # controla si el programa esta en ejecucion
         self.intSlider = 0.5
 
@@ -34,6 +41,7 @@ class GraphiBubbleShort():
         self.rellemarVectorNum()
         """Se muestra todo"""
         self.visualizarInterfaz()
+
 
     def visualizarInterfaz(self):
         self.pantalla.title("Burbuja by loko")
@@ -47,6 +55,11 @@ class GraphiBubbleShort():
 
         """Se procede a graficar el vector"""
         self.graficarVector()
+
+        # Se añade un evento al bucle de eventos para que tan pronto
+        # como arranque mainloop() lo procese. Ese evento consiste en 
+        # invocar la función self.update_graphic lo antes posible (tras 0ms)
+        self.pantalla.after(0, self.update_graphic)
         self.pantalla.mainloop()
 
     def rellemarVectorNum(self):
@@ -55,10 +68,24 @@ class GraphiBubbleShort():
             for i in range(320):
                 self.vectorNumeros.append(randint(1, 400))
 
+    def update_graphic(self):
+        """Esta función actualiza el estado de la gráfica cada 30ms"""
+
+        # Solo la actualizamos si realmente está cambiando, para ahorrar CPU
+        if self.estoyRondando:
+            self.graficarVector()
+            # Cambiamos también la etiqueta con el número de iteraciones
+            self.lblIteracion["text"] = "Iteracion: " + str(self.Iteracion)
+        self.pantalla.after(30, self.update_graphic)
+
     def graficarVector(self):
+        # Borramos el canvas y lo repintamos
+        self.telaGraficas.delete("vector")
         for i in range(0, len(self.vectorNumeros)):
-            x0 = 1 + (2*i)
-            self.telaGraficas.create_rectangle(x0, 0, x0+1, self.vectorNumeros[i], tags="vector")
+            x0 = 1 + (2 * i)
+            self.telaGraficas.create_rectangle(
+                x0, 0, x0 + 1, self.vectorNumeros[i], tags="vector"
+            )
 
     def play(self):
         self.pantalla.title("Burbuja by loko:PLAY")
@@ -72,33 +99,27 @@ class GraphiBubbleShort():
         contadori = 0
         contadorj = 0
         aux = 0
-        Iteracion = 0
+        # La variable Iteracion pasa a ser un atributo del objeto, para poder
+        # compartirla con la función que actualiza la interfaz
+        self.Iteracion = 0
         while True:
             while self.estoyRondando and contadori < len(self.vectorNumeros):
                 # procedo a instaciar j en lo que no esta ordenado
                 contadorj = 0
-                # procedo a borrar los numeros
-                self.telaGraficas.delete("vector")
-                while self.estoyRondando and contadorj < len(self.vectorNumeros)-(contadori+1):
-                    if self.vectorNumeros[contadorj] < self.vectorNumeros[contadorj+1]:
-                        aux = self.vectorNumeros[contadorj+1]
-                        self.vectorNumeros[contadorj+1] = self.vectorNumeros[contadorj]
+                while self.estoyRondando and contadorj < len(self.vectorNumeros) - (contadori + 1):
+                    if (self.vectorNumeros[contadorj] < self.vectorNumeros[contadorj + 1]):
+                        aux = self.vectorNumeros[contadorj + 1]
+                        self.vectorNumeros[contadorj + 1] = self.vectorNumeros[contadorj]
                         self.vectorNumeros[contadorj] = aux
-                    
+
                     if self.estoyRondando:
-                        Iteracion = Iteracion + 1
-                        self.lblIteracion['text'] = "Iteracion: "+str(Iteracion)
+                        self.Iteracion += 1
                         contadorj = contadorj + 1
-                
-                # se grafica el vector
-                self.graficarVector()
-                self.telaGraficas.update_idletasks()
-                
-                sleep(0.1)
+
+                sleep(0.01)
                 """Se incrementa i"""
                 if self.estoyRondando:
                     contadori = contadori + 1
-
 
 
 sw = GraphiBubbleShort()
